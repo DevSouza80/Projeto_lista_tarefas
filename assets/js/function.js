c6a9fc499edba825aa1913cb7cb548635f4ad91f
Pedro Henrique Dias de Souza <desenvolvedorweb7070@hotmail.com>
2026-06-09 15:57:38 -0300

    Update function.js

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,65 +1,63 @@
-let listElement = document.querySelector("#app ul");
-let inputElement = document.querySelector("#app input");
-let buttonElement = document.querySelector("#app button");
+const listElement = document.querySelector("#wrapper-container ul");
+const inputElement = document.querySelector("#wrapper-container input");
+const buttonElement = document.querySelector("#wrapper-container button");
 
-let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
+let tarefa = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
 
-function renderTarefas() {
-    listElement.innerHTML = ""; // Irá esvaziar tudo que estiver dentro da lista de tarefas
+function rendeTarefa(todo) {
+     listElement.innerHTML = "";
 
-    tarefas.map((todo) => {
-       let liElement = document.createElement("li"); // Nossa li
-       let tarefaText = document.createTextNode(todo); // Nosso texto
+     tarefa.map((todo) => {
+        let liElement = document.createElement("li");
+        let TarefaText = document.createTextNode(todo);
 
-       let linkElement = document.createElement("a"); // Criamos um link a, onde será o botão de excluir
-       linkElement.setAttribute("href",'#'); // Adicionamos atributos e # para esse href não levar para nenhuma aba externa
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href","#");
 
-       let linkText = document.createTextNode("Excluir"); // Criamos um nome para a tag a, será o nome excluir, usaremos com a tag a para servi de botão para deletar itens da lista
-       linkElement.appendChild(linkText);// pegamos a ancora a e colocamos o texto dentro
-
-       let posicao = tarefas.indexOf(todo); // Serve para pegar a posição da lista
-
-       linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`);// Colocamos o atributo index dentro da função de deletar
+        let linkText = document.createTextNode("Excluir");
 
 
-       liElement.appendChild(tarefaText); // Colocamos o texto dentro da li.
-       liElement.appendChild(linkElement); // Colocamos dentro da li, também nosso botão de excluir
-       listElement.appendChild(liElement); // Colocamos nossa li dentro da ul.
+        liElement.appendChild(TarefaText);
+        listElement.appendChild(liElement);
 
-       salvarDados();
+         let posicao = tarefa.indexOf(todo);
 
-    });
+         linkElement.setAttribute("onclick",`deletarTarefa(${posicao})`);
 
+          liElement.appendChild(linkElement);
+         linkElement.appendChild(linkText);
+     })
 }
 
-renderTarefas(); // Irá renderiza todas as tarefas que estiver na lista, logo quando acessar a página ou se estiver vazia irá manter.
 
-function adicionarTarefas() { // Função para cadastrar uma nova tarefa
-    if(inputElement.value === '') {  // Se o usuário não digitar nada, irá aparecer o alert ! || Valor vazio
-        alert("Digite alguma tarefa!");
+rendeTarefa();
+
+function adicionarTarefa() {
+    if(inputElement.value === "") {
+        alert("Digite alguma coisa!");
         return false;
     } else {
-        let novaTarefa = inputElement.value; // Criei uma variavel para armazenar o que o  usuário digitar
-        
-        tarefas.push(novaTarefa); // Metodo push => está responsavel por adicionar o que o usuario digitou no array da tarefa
+        let novaTarefa = inputElement.value;
+
+        tarefa.push(novaTarefa);
 
         inputElement.value = '';
 
-        renderTarefas();
+        rendeTarefa();
         salvarDados();
     }
 }
 
-buttonElement.onclick = adicionarTarefas;
 
+buttonElement.onclick = adicionarTarefa;
 
 function deletarTarefa(posicao) {
-    tarefas.splice(posicao,1);
+    tarefa.splice(posicao,1);
 
-    renderTarefas();
+    rendeTarefa();
     salvarDados();
 }
 
 function salvarDados() {
-    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas)); // Salvar itens no localStorage, primeiro passamos a chave KEY e depois a lista de tarefas(valor)
+    localStorage.setItem("@listaTarefas",JSON.stringify(tarefa));
 }
